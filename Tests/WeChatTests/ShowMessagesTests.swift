a393799a25cca7c588fec4faa378c5360cb44ed9
Qiqi Jin <ginqi7@gmail.com>
2025-05-26 15:54:26 +0800

    Test: fix test error.

--- Tests/WeChatTests/ShowMessagesTests.swift
+++ Tests/WeChatTests/ShowMessagesTests.swift
@@ -1,3 +1,5 @@
+import Accessibility
+import ApplicationServices
 import Foundation
 import XCTest
 
@@ -5,14 +7,26 @@ import XCTest
 
 final class ShowMessagesTests: XCTestCase {
   func testToStr() throws {
-    let message1 = Message(user: "我", message: "I created a WeChat CLI tool.", index: 0, date: "")
-    let message2 = Message(user: "user1", message: "Looks good.", index: 1, date: "")
-    let message3 = Message(user: "user1", message: "发送了一个图片", index: 2, date: "11:55")
+    let message1 = Message(
+      user: "我", message: "I created a WeChat CLI tool.", index: 0, date: "",
+      element: AXUIElementCreateApplication(1))
+    let message2 = Message(
+      user: "user1", message: "Looks good.", index: 1, date: "",
+      element: AXUIElementCreateApplication(1))
+    let message3 = Message(
+      user: "user1", message: "发送了一个图片", index: 2, date: "11:55",
+      element: AXUIElementCreateApplication(1))
     let message4 = Message(
-      user: "user1", message: "This marks a new beginning.", index: 3, date: "12:12")
-    let message5 = Message(user: "user1", message: "发送了一个图片", index: 4, date: "12:12")
-    let message6 = Message(user: "user1", message: "哈哈", index: 5, date: "12:12")
-    let message7 = Message(user: "我", message: "哈哈", index: 6, date: "12:12")
+      user: "user1", message: "This marks a new beginning.", index: 3, date: "12:12",
+      element: AXUIElementCreateApplication(1))
+    let message5 = Message(
+      user: "user1", message: "发送了一个图片", index: 4, date: "12:12",
+      element: AXUIElementCreateApplication(1))
+    let message6 = Message(
+      user: "user1", message: "哈哈", index: 5, date: "12:12",
+      element: AXUIElementCreateApplication(1))
+    let message7 = Message(
+      user: "我", message: "哈哈", index: 6, date: "12:12", element: AXUIElementCreateApplication(1))
 
     var messages: [Message] = []
     messages.append(message1)
@@ -22,7 +36,7 @@ final class ShowMessagesTests: XCTestCase {
     messages.append(message5)
     messages.append(message6)
     messages.append(message7)
-    let chatInfo = ChatInfo(title: "user1", element: AXUIElementCreateSystemWide(), index: 0)
+    let chatInfo = ChatInfo(title: "user1", element: AXUIElementCreateApplication(1), index: 0)
     chatInfo.messages = messages
     if let content = readFile() {
       XCTAssertEqual(content, chatInfo.messagesToStr())
